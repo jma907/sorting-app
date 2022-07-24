ad3f02891cffe0507118f49d03e76922aaeea04a
Justin <justinma907@gmail.com>
2022-07-24 00:07:38 -0500

    Set sort bar positioning and format

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -16,8 +16,8 @@ export default class SortingVisualizer extends React.Component {
 
     resetArray() {
         const array = [];
-        for (let i = 0; i < 100; i++) {
-            array.push(randomIntFromInterval(10, 1000))
+        for (let i = 0; i < 400; i++) {
+            array.push(randomIntFromInterval(10, 800))
         }
         this.setState({array})
     }
@@ -26,13 +26,15 @@ export default class SortingVisualizer extends React.Component {
         const {array} = this.state;
 
         return (
-            <>
+            <div className="array-container">
                 {array.map((value, idx) => (
-                    <div className="array-bar" key={idx}>
-                        {value}
+                    <div 
+                        className="array-bar" 
+                        key={idx}
+                        style={{height: `${value}px`}}>
                     </div>
                 ))}
-            </>
+            </div>
         );
     }
 }
