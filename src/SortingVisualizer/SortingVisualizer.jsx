21e6374d0d2c74e7d62b4d52a15c0387144ebd70
Justin <justinma907@gmail.com>
2022-07-24 01:17:50 -0500

    Add sub container to fix formatting
    Sub container contains each bar of the array to be sorted. Intead of each bar being its own container on the same level, the entire collection of bars is on the same level as the buttons. This allows the bars to be side by side horizontally while the buttons are vertical.

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -27,13 +27,16 @@ export default class SortingVisualizer extends React.Component {
 
         return (
             <div className="array-container">
-                {array.map((value, idx) => (
-                    <div 
-                        className="array-bar" 
-                        key={idx}
-                        style={{height: `${value}px`}}>
-                    </div>
-                ))}
+                <div className="array-holder">
+                    {array.map((value, idx) => (
+                        <div 
+                            className="array-bar" 
+                            key={idx}
+                            style={{height: `${value}px`}}>
+                        </div>
+                    ))}
+                </div>
+                <button onClick={() => this.resetArray()}>Randomize Array</button>
             </div>
         );
     }
