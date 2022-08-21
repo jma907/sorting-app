179a99c2a87deb2662c54faa02b5e3110da29e9e
Justin <justinma907@gmail.com>
2022-08-21 00:56:04 -0500

    Remake structure of program and randomizer

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,112 +2,42 @@ import React from 'react';
 import './SortingVisualizer.css'
 import {getMergeAnimations} from '../SortingAlgorithms/SortingAlgorithms.js';
 import {getBubbleAnimations} from '../SortingAlgorithms/SortingAlgorithms.js';
+import { useState, useEffect, useRef } from 'react';
 
-const ANIMATION_SPEED_MS = 1;
-
+ARRAY_LENGTH = 100;
+const MIN = 5;
+const MAX = 80;
+const ANIMATION_SPEED_MS = 5;
 const PRIMARY_COLOR = 'coral' 
-
 const SECONDARY_COLOR = 'blue'
 
-export default class SortingVisualizer extends React.Component {
-    constructor(prop) {
-        super(prop);
+export default function SortVisualizer(props) {
+    const [arr, setArr] = useState([]);
+    const [sorting, setSorting] = useState(false);
+    const [sorted, setSorted] = useState(false);
+    const containerRef = useRef(null);
 
-        this.state = {
-            array: [],
-        };
-    }
+    useEffect(initializeArray, []);
 
-    componentDidMount() {
-        this.resetArray();
-    }
-
-    resetArray() {
+    function initializeArray() {
+        if (sorting) return;
+        if (sorted) resetArrayColor();
+        setSorted(false);
         const array = [];
-        for (let i = 0; i < 500; i++) {
-            array.push(randomIntFromInterval(10, 800))
+        for (let i = 0; i < 100; i++) {
+            array.push((MAX - MIN) * (i / ARRAY_LENGTH) + MIN);
         }
-        this.setState({array})
+        resetArray(array);
+        setArr(array);
     }
-
-    bubbleSort() {
-        const animations = getBubbleAnimations(this.state.array);
-        for (let i = 0; i < animations.length; i++) {
-        const arrayBars = document.getElementsByClassName('array-bar');
-        const isColorChange = i % 3 !== 2;
-        if (isColorChange) {
-            const [barOneIdx, barTwoIdx] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-            setTimeout(() => {
-            barOneStyle.backgroundColor = color;
-            barTwoStyle.backgroundColor = color;
-            }, i * ANIMATION_SPEED_MS);
-        } else {
-            setTimeout(() => {
-            const [barOneIdx, newHeight] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            barOneStyle.height = `${newHeight}px`;
-            }, i * ANIMATION_SPEED_MS);
-        }
-        }
-    }
-
-    exchangeSort() {}
-
-    mergeSort() {
-        const animations = getMergeAnimations(this.state.array);
-        for (let i = 0; i < animations.length; i++) {
-        const arrayBars = document.getElementsByClassName('array-bar');
-        const isColorChange = i % 3 !== 2;
-        if (isColorChange) {
-            const [barOneIdx, barTwoIdx] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            const barTwoStyle = arrayBars[barTwoIdx].style;
-            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
-            setTimeout(() => {
-            barOneStyle.backgroundColor = color;
-            barTwoStyle.backgroundColor = color;
-            }, i * ANIMATION_SPEED_MS);
-        } else {
-            setTimeout(() => {
-            const [barOneIdx, newHeight] = animations[i];
-            const barOneStyle = arrayBars[barOneIdx].style;
-            barOneStyle.height = `${newHeight}px`;
-            }, i * ANIMATION_SPEED_MS);
-        }
-        }
-    }
-
-    countingSort() {}
-
-    render() {
-        const {array} = this.state;
-
-        return (
-            <div className="array-container">
-                <div className="array-holder">
-                    {array.map((value, idx) => (
-                        <div 
-                            className="array-bar" 
-                            key={idx}
-                            style={{height: `${value}px`}}>
-                        </div>
-                    ))}
-                </div>
-                <div className="button-holder">
-                    <button onClick={() => this.resetArray()}>Randomize Array</button>
-                    <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
-                    <button onClick={() => this.exchangeSort()}>Exchange Sort</button>
-                    <button onClick={() => this.mergeSort()}>Merge Sort</button>
-                    <button onClick={() => this.countingSort()}>Counting Sort</button>
-                </div>
-            </div>
-        );
-    }
+    
 }
 
-function randomIntFromInterval(low, high) {
-    return Math.floor(Math.random() * (high - low + 1) + low);
+const resetArray = (array) => {
+    for (let i = array.length - 1; i >= 0; i++) {
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[randomIndex];
+        array[randomIndex] = temp;
+    } 
 }
